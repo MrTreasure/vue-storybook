a05b454f9f02bed6679208ff66188bc669291d51
Treasure <treasurewmj@foxmail.com>
2019-07-12 01:25:07 +0800

    feat:添加HelloWorld组件注册

--- packages/HelloWorld/index.ts
+++ packages/HelloWorld/index.ts
@@ -1,3 +1,8 @@
 import HelloWorld from './src/HelloWorld.vue'
+import { VueConstructor } from 'vue'
+
+(HelloWorld as any).install = function(Vue: VueConstructor) {
+  Vue.component(HelloWorld.name, HelloWorld)
+}
 
 export default HelloWorld
